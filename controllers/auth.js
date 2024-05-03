c17ca4b92b1937b22da74eff7a8c9df35c297b9f
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-05-03 20:49:48 +0200

    secret_key added

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
