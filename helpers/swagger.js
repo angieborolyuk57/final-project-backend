69b1b3d38f8305ea76a3325d05c3f3833ed7dac4
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-05-08 20:15:51 +0200

    error fixed

--- helpers/swagger.js
+++ helpers/swagger.js
@@ -1,8 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const swaggerDocs = async () => {
-  return JSON.parse(await fs.readFile(`${path.resolve()}/swagger.json`));
-};
+const swaggerDocs = async () =>
+  JSON.parse(await fs.readFile(`${path.resolve()}/swagger.json`));
 
 module.exports = swaggerDocs;
